4f04ae8f0bde58eed81ceda2c587aa8d31b91788
shahinul <shahinul37@outlook.com>
2022-02-01 17:12:21 +0600

    guessing game edited

--- guessing-game.js
+++ guessing-game.js
@@ -1,9 +1,7 @@
 
 let num = parseInt(prompt("Enter your number: "))
 
-
 let guessNumber = 10*(Math.random());
 guessNumber = Math.floor(guessNumber);
-
 let result = (num == guessNumber)? 'You win':'You lost'
 console.log(result);
