fcd6a69d2ce1f455203a677278969d82f73c1366
shahinul <shahinul37@outlook.com>
2022-02-03 00:21:55 +0600

    objects basic practise

---
+++ object-basic.js
@@ -0,0 +1,12 @@
+//একটা অবজেক্ট ডিক্লেয়ার করবে। সেটাতে তিনটা প্রপার্টি থাকবে। এবং কোন একটা প্রোপার্টি এর মান চেইঞ্জ করবা
+
+let phone = {
+    name: "samsung",
+    price: 22000,
+    ram: 16,
+    color: 'silver',
+    camera: 48
+}
+
+phone.color = "red";
+console.log(phone)
