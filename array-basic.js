e094baba46da5efaf4db51cad7455ebe45217c2e
shahinul <shahinul37@outlook.com>
2022-02-03 00:12:22 +0600

    array basice practice

---
+++ array-basic.js
@@ -0,0 +1,11 @@
+let myArray = ['apple', 54, "mim", "orange", "banana", "mango", "grapes"]
+
+myArray[3] = "tamarind";
+myArray.push("shahin", "limon");
+myArray.pop();
+console.log(myArray)
+
+for (let i = 0;  i < myArray.length; i++) {
+    console.log(myArray[i])
+    
+}
