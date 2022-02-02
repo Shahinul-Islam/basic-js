1fc04b53eff02aa4277422737e33bf72b3da3705
shahinul <shahinul37@outlook.com>
2022-02-03 00:18:44 +0600

    product of three numbers

---
+++ productOfThree.js
@@ -0,0 +1,6 @@
+//তিনটা সংখ্যার গুনফল বের করে ফাইনাল রেজাল্ট আউটপুট হিসেবে রিটার্ন করতে হবে। তুমি কি সেই কোড লিখতে পারবে। যদি পারো তাহলে সেই কোড লিখে ফেলো। 
+
+function productOfThree(num1, num2, num3) {
+    return num1*num2*num3
+}
+console.log(productOfThree(5,6,10))
